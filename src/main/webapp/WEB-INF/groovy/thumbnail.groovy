79fde5b0704fcf3dd990df61b7acc34e88852e0b
Ben Fortuna <benfortuna@gmail.com>
2013-03-27 10:21:44 +1100

    Update thumbnail.groovy
    Support thumbnail images

--- src/main/webapp/WEB-INF/groovy/thumbnail.groovy
+++ src/main/webapp/WEB-INF/groovy/thumbnail.groovy
@@ -9,11 +9,15 @@ Session session = repository.login(new SimpleCredentials('readonly', ''.toCharAr
 
 if (request.getParameter('p')) {
   def path = request.getParameter('p')
+  def thumbnailProperty = session.getNode(path)['mn:thumbnail']
   def favIconProperty = session.getNode(path).parent['mn:icon']
-  if (favIconProperty) {
+  if (thumbnailProperty) {
+  	redirect thumbnailProperty.string
+  }
+  else if (favIconProperty) {
     sout << favIconProperty.binary.stream
   }
   else {
-	  redirect '/images/fh48.png'
+	redirect '/images/fh48.png'
   }
 }
