e4d446076e64c58ad897b84a05e3806dfadc7bc0
Ben Fortuna <benfortuna@gmail.com>
2013-03-26 12:48:01 +1100

    Update thumbnail.groovy

--- src/main/webapp/WEB-INF/groovy/thumbnail.groovy
+++ src/main/webapp/WEB-INF/groovy/thumbnail.groovy
@@ -1,3 +1,12 @@
+import javax.jcr.Session
+import javax.jcr.SimpleCredentials
+import javax.naming.InitialContext
+
+def context = new InitialContext()
+def repository = context.lookup('feedhabit')
+
+Session session = repository.login(new SimpleCredentials('readonly', ''.toCharArray()))
+
 if (request.getParam('p')) {
   def path = request.getParam('p')
   def favIconProperty = session.getNode(path).parent['mn:icon']
