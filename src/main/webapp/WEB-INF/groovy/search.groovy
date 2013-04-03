1831d5617d26c63e7a95b240902cf0db9253b501
Ben Fortuna <benfortuna@gmail.com>
2013-04-03 23:40:47 +1100

    Added search

---
+++ src/main/webapp/WEB-INF/groovy/search.groovy
@@ -0,0 +1,8 @@
+import javax.jcr.query.Query
+
+if (request.getParameter('q')) {
+	def searchQuery = request.jcrSession.workspace.queryManager.createQuery("//*[jcr:contains(., '${request.getParameter('q')}')]/(@mn:text|rep:excerpt(.))", Query.XPATH)
+	 request.setAttribute('query', searchQuery)
+ 
+}
+forward 'search.html'
