af025d84fc3844031d31e9a75158320ffb7b7f14
Ben Fortuna <benfortuna@gmail.com>
2013-04-04 00:21:37 +1100

    Added search field

--- src/main/webapp/WEB-INF/groovy/search.groovy
+++ src/main/webapp/WEB-INF/groovy/search.groovy
@@ -4,5 +4,8 @@ if (request.getParameter('q')) {
 	def searchQuery = request.jcrSession.workspace.queryManager.createQuery("//*[jcr:contains(., '${request.getParameter('q')}')]/(@mn:text|rep:excerpt(.))", Query.XPATH)
 	 request.setAttribute('query', searchQuery)
  
+	 forward 'search.html'
 }
-forward 'search.html'
+else {
+	redirect 'index.groovy'
+}
